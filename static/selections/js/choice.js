934ab9acd8404badcf03e84ddce45369046e8374
Kim mingi <kmk2737@snu.ac.kr>
2022-09-14 18:47:46 +0900

    final commit

--- static/selections/js/choice.js
+++ static/selections/js/choice.js
@@ -2,7 +2,13 @@ var photos = [
     "https://image.musinsa.com/mfile_s01/_shopstaff/view.staff_631930d56bfac.jpg?20220908091426",
     "https://image.musinsa.com/mfile_s01/_shopstaff/view.staff_63195a58f4161.jpg?20220908120258",
     "https://image.musinsa.com/mfile_s01/_shopstaff/view.staff_63195a5a7545e.jpg?20220908120258",
-    "https://image.musinsa.com/mfile_s01/_shopstaff/view.staff_63195a51351a7.jpg?20220908120258"
+    "https://image.musinsa.com/mfile_s01/_shopstaff/view.staff_63195a51351a7.jpg?20220908120258",
+    "https://image.musinsa.com/mfile_s01/_shopstaff/view.staff_6321899ec2d9f.jpg?20220914170028",
+    "https://image.musinsa.com/mfile_s01/_shopstaff/view.staff_6321890d24740.jpg?20220914165633",
+    "https://image.musinsa.com/mfile_s01/_shopstaff/view.staff_632187b6b268f.jpg?20220914165100",
+    "https://image.musinsa.com/mfile_s01/_shopstaff/view.staff_6321815810a98.jpg?20220914163804",
+    "https://image.musinsa.com/mfile_s01/_shopstaff/view.staff_6321809981358.jpg?20220914163434",
+    "https://image.musinsa.com/mfile_s01/_shopstaff/view.staff_632178b976206.jpg?20220914161429"
 ]
 
 $(function(){
